4ffbfcb5d1b933456f11be314fca9955e73f5129
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-05-11 00:20:38 +0600

    newsletter section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './../../components/navbar/Navbar';
 import Banner from '../../components/banner/Banner';
+import NewsLetter from '../newsletter/NewsLetter';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <NewsLetter></NewsLetter>
         </div>
     );
 };

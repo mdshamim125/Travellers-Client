f030347dd17a7aed3161df7e17820c49ad9d9b1e
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-05-15 20:35:16 +0600

    featured-blogs page added

--- src/pages/blog-details/Comments.jsx
+++ src/pages/blog-details/Comments.jsx
@@ -1,16 +1,11 @@
-const Comments = ({ comments }) => {
-  console.log(comments);
-  const { name, profile, comment } = comments;
-  return (
-    <div>
-      <div>
+import React from 'react';
+
+const Comments = () => {
+    return (
         <div>
-          <img src={profile} alt="" className="w-10"/>
-          <p>{comments.name}</p>
+            
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Comments;
--- src/pages/featured-blogs/FeaturedBlogs.jsx
+++ src/pages/featured-blogs/FeaturedBlogs.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const FeaturedBlogs = () => {
+  const [topBlogs, setTopBlogs] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/featured-blogs`
+        );
+
+        const sortedBlogs = response.data
+          .map((blog) => ({
+            ...blog,
+            wordCount: blog.long_description.split(" ").length,
+          }))
+          .sort((a, b) => b.wordCount - a.wordCount);
+
+        const topTenBlogs = sortedBlogs.slice(0, 10);
+
+        setTopBlogs(topTenBlogs);
+      } catch (error) {
+        console.error("Error fetching top blogs: ", error);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div>
-      <div className="min-h-[calc(100vh-250px)] my-12"></div>
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl text-center font-semibold mb-6">
+        Featured Blogs
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          <thead>
+            <tr>
+              <th>Serial Number</th>
+              <th>Blog Title</th>
+              <th>Blogger Name</th>
+              <th>Blogger Profile</th>
+            </tr>
+          </thead>
+          <tbody>
+            {topBlogs.map((blog, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{blog.title}</td>
+                <td>{blog.blog_owner}</td>
+                <td>
+                  <img
+                    src={blog.blog_owner_profile}
+                    alt="Profile"
+                    className="w-10 h-10 rounded-full"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
